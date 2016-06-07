17ff61f0958e76f49b44e89b311af9e93a1e4219
frantic1048 <archer@frantic1048.com>
2016-06-07 21:00:34 +0800

    logout

--- test/spec/loginLogout.js
+++ test/spec/loginLogout.js
@@ -1,13 +1,13 @@
 import { it, describe } from 'selenium-webdriver/testing';
 import chai, { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import { By } from 'selenium-webdriver';
+import { By, until } from 'selenium-webdriver';
 
 chai.use(chaiAsPromised);
 
 export default (driver, baseURL) =>
 describe('Login - Logout', () => {
-  it('Login', (done) => {
+  it('Login - Logout', (done) => {
     // open index
     driver.get(`${baseURL}/`);
     expect(driver.getTitle()).to.eventually.equal('人力资源管理系统');
@@ -25,6 +25,25 @@ describe('Login - Logout', () => {
     expect(driver.getTitle()).to.eventually.equal('企业信息管理系统');
     expect(driver.getCurrentUrl())
       .to.eventually.equal(`${baseURL}/ad_home.php`)
+      .and.notify(done);
+
+    // we are logged in
+    // click the logout button
+    driver.findElement(By.css('#user-nav>ul>li:nth-child(2) a[href="/anli/logout.php"]')).click();
+
+    // wait alert shows
+    driver.wait(until.alertIsPresent());
+
+
+    // siwtch to alert window
+    // and accept it
+    driver.switchTo().alert().accept();
+
+    // switch bach to main window
+    driver.switchTo().defaultContent();
+    // check if we are back to login page
+    expect(driver.getCurrentUrl())
+      .to.eventually.equal(`${baseURL}/login_page.php`)
       .and.notify(done);
   });
 });
