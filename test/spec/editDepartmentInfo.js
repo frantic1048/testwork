1f38073191dc4c00fece467494e5142414ddb71e
VickyTse <vickyshaun36@163.com>
2016-06-16 10:43:02 +0800

    【提交测试js】修正语法问题

--- test/spec/editDepartmentInfo.js
+++ test/spec/editDepartmentInfo.js
@@ -1,14 +1,12 @@
 import { it, describe } from 'selenium-webdriver/testing';
-import chai, { expect } from 'chai';
-import chaiAsPromised from 'chai-as-promised';
+import { expect } from 'chai';
 import { By, until } from 'selenium-webdriver';
-
-chai.use(chaiAsPromised);
 
 export default (driver, baseURL) =>
 <<<<<<< 904a2aa56b53347adce26aa57f46562097eeaa45
 <<<<<<< 9302e24c85ddb8b7d31196adad9bc67da8c135c4
 describe('editDepartmentInfo', () => {
+<<<<<<< 98038c8819162ae03cd6877f60be34f2afb9cc8f
   it('editDepartmentInfo', (done) => {
 =======
 describe('checkDepartmentInfo', () => {
@@ -18,58 +16,60 @@ describe('checkDepartmentInfo', () => {
 describe('editDepartmentInfo', () => {
   it('editDepartmentInfo', (done) => {
 >>>>>>> 【提交测试js】填报月报人事、填报月报绩效
+=======
+  it('editDepartmentInfo', async () => {
+>>>>>>> 【提交测试js】修正语法问题
     // open index
-    driver.get(`${baseURL}/`);
+    await driver.get(`${baseURL}/`);
     // expect(driver.getTitle()).to.eventually.equal('人力资源管理系统');
 
     // input username
-    driver.findElement(By.css('input[name="name"]')).sendKeys('test');
+    await driver.findElement(By.css('input[name="name"]')).sendKeys('test');
 
     // input password
-    driver.findElement(By.css('input[name="password"]')).sendKeys('test');
+    await driver.findElement(By.css('input[name="password"]')).sendKeys('test');
 
     // press login button
-    driver.findElement(By.css('input[type="submit"]')).click();
+    await driver.findElement(By.css('input[type="submit"]')).click();
 
-    driver.findElement(By.css('#sidebar > ul > li:nth-child(2) > a')).click();
+    await driver.findElement(By.css('#sidebar > ul > li:nth-child(2) > a')).click();
 
     const elem0 = driver.findElement(By.css('#breadcrumb > a.current'));
-    expect(elem0.getText())
-      .to.eventually.equal('部门信息')
-      .and.notify(done);
+    expect(await elem0.getText()).to.equal('部门信息');
 
     //click edit button
-    driver.findElement(By.css('#content > div.container-fluid > div > div > a > button')).click();
+    await driver.findElement(By.css('#content > div.container-fluid > div > div > a > button')).click();
 
-    driver.findElement(By.css('input[name="department_name"]')).sendKeys('财务部修改');
+    await driver.findElement(By.css('input[name="department_name"]')).sendKeys('财务部修改');
 
-    driver.findElement(By.css('#editForm > div.form-actions > button')).click();
+    await driver.findElement(By.css('#editForm > div.form-actions > button')).click();
 
     //check edit
     const elem1 = driver.findElement(By.css('#content > div.container-fluid '
       +' > div > div > div > div.widget-content > table:nth-child(3) '
       +' > tbody > tr:nth-child(1) > td:nth-child(2)'));
-    expect(elem1.getText())
-      .to.eventually.equal('财务部修改')
-      .and.notify(done);
+    expect(await elem1.getText()).to.equal('财务部修改');
 
     // we are logged in
     // click the logout button
-    driver.findElement(By.css('#user-nav>ul>li:nth-child(2) a[href="/anli/logout.php"]')).click();
+    await driver.findElement(By.css('#user-nav>ul>li:nth-child(2) a[href="/anli/logout.php"]'))
+      .click();
 
     // wait alert shows
-    driver.wait(until.alertIsPresent());
+    await driver.wait(until.alertIsPresent());
 
 
     // siwtch to alert window
     // and accept it
-    driver.switchTo().alert().accept();
+    await driver.switchTo().alert().accept();
 
     // switch bach to main window
-    driver.switchTo().defaultContent();
+    await driver.switchTo().defaultContent();
     // check if we are back to login page
-    expect(driver.getCurrentUrl())
-      .to.eventually.equal(`${baseURL}/login_page.php`)
-      .and.notify(done);
+    expect(await driver.getCurrentUrl())
+      .to.equal(`${baseURL}/login_page.php`);
+
+    // statments are now synchronous
+    // we don't need to manually call done() now XD
   });
 });
--- test/spec/searchMonthData.js
+++ test/spec/searchMonthData.js
@@ -1,14 +1,12 @@
 import { it, describe } from 'selenium-webdriver/testing';
-import chai, { expect } from 'chai';
-import chaiAsPromised from 'chai-as-promised';
+import { expect } from 'chai';
 import { By, until } from 'selenium-webdriver';
-
-chai.use(chaiAsPromised);
 
 export default (driver, baseURL) =>
 <<<<<<< 904a2aa56b53347adce26aa57f46562097eeaa45
 <<<<<<< 9302e24c85ddb8b7d31196adad9bc67da8c135c4
 describe('searchMonthData', () => {
+<<<<<<< 98038c8819162ae03cd6877f60be34f2afb9cc8f
   it('searchMonthData', (done) => {
 =======
 describe('checkDepartmentInfo', () => {
@@ -18,55 +16,57 @@ describe('checkDepartmentInfo', () => {
 describe('searchMonthData', () => {
   it('searchMonthData', (done) => {
 >>>>>>> 【提交测试js】填报月报人事、填报月报绩效
+=======
+  it('searchMonthData', async () => {
+>>>>>>> 【提交测试js】修正语法问题
     // open index
-    driver.get(`${baseURL}/`);
+    await driver.get(`${baseURL}/`);
     // expect(driver.getTitle()).to.eventually.equal('人力资源管理系统');
 
     // input username
-    driver.findElement(By.css('input[name="name"]')).sendKeys('test');
+    await driver.findElement(By.css('input[name="name"]')).sendKeys('test');
 
     // input password
-    driver.findElement(By.css('input[name="password"]')).sendKeys('test');
+    await driver.findElement(By.css('input[name="password"]')).sendKeys('test');
 
     // press login button
-    driver.findElement(By.css('input[type="submit"]')).click();
+    await driver.findElement(By.css('input[type="submit"]')).click();
 
-    driver.findElement(By.css('#sidebar > ul > li:nth-child(5) > a > span')).click();
+    await driver.findElement(By.css('#sidebar > ul > li:nth-child(5) > a > span')).click();
 
     const elem0 = driver.findElement(By.css('#breadcrumb > a.current'));
-    expect(elem0.getText())
-      .to.eventually.equal('月报查询')
-      .and.notify(done);
+    expect(await elem0.getText()).to.equal('月报查询');
 
     //click search button
-    driver.findElement(By.css('#content > div.container-fluid > div > div >'
+    await driver.findElement(By.css('#content > div.container-fluid > div > div >'
       +' div > div.widget-content.nopadding > table > tbody > tr:nth-child(2) '
       +'> td:nth-child(2) > a > span')).click();
 
     //check search
     const elem1 = driver.findElement(By.css('#content > div.container-fluid >'
       +' div > div.span8 > div > div.widget-title > h5'));
-    expect(elem1.getText())
-      .to.eventually.equal('绩效考核')
-      .and.notify(done);
+    expect(await elem1.getText()).to.equal('绩效考核');
 
     // we are logged in
     // click the logout button
-    driver.findElement(By.css('#user-nav>ul>li:nth-child(2) a[href="/anli/logout.php"]')).click();
+     await driver.findElement(By.css('#user-nav>ul>li:nth-child(2) a[href="/anli/logout.php"]'))
+      .click();
 
     // wait alert shows
-    driver.wait(until.alertIsPresent());
+    await driver.wait(until.alertIsPresent());
 
 
     // siwtch to alert window
     // and accept it
-    driver.switchTo().alert().accept();
+    await driver.switchTo().alert().accept();
 
     // switch bach to main window
-    driver.switchTo().defaultContent();
+    await driver.switchTo().defaultContent();
     // check if we are back to login page
-    expect(driver.getCurrentUrl())
-      .to.eventually.equal(`${baseURL}/login_page.php`)
-      .and.notify(done);
+    expect(await driver.getCurrentUrl())
+      .to.equal(`${baseURL}/login_page.php`);
+
+    // statments are now synchronous
+    // we don't need to manually call done() now XD
   });
 });
