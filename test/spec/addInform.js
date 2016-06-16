4fed45b0642308644ea8a955bca86a9e673d55af
diwujiahao <949447319@qq.com>
2016-06-16 11:15:58 +0800

    【纠错】

--- test/spec/addInform.js
+++ test/spec/addInform.js
@@ -6,7 +6,7 @@ export default (driver, baseURL) =>
 describe('Add - Inform', () => {
   it('Add - Inform', async () => {
     await driver.get(`${baseURL}/`);
-    expect(await driver.getTitle()).to.eventually.equal('人力资源管理系统');
+    expect(await driver.getTitle()).to.equal('人力资源管理系统');
 
     await driver.findElement(By.css('input[name="name"]')).sendKeys('admin');
 
@@ -14,28 +14,25 @@ describe('Add - Inform', () => {
 
     await driver.findElement(By.css('input[type="submit"]')).click();
 
-    (await driver.getTitle()).to.eventually.equal('企业信息管理系统');
+    (await driver.getTitle()).to.equal('企业信息管理系统');
     expect(await driver.getCurrentUrl())
-        .to.eventually.equal(`${baseURL}/ad_home.php`)
-        .and.notify(done);
+        .to.equal(`${baseURL}/ad_home.php`);
 
     await driver.findElement(By.css('#content >' +
         ' div.container-fluid > div > ul >' +
         ' li.bg_lo.span3 > a')).click();
 
-    expect(await driver.getTitle()).to.eventually.equal('企业信息管理系统');
+    expect(await driver.getTitle()).to.equal('企业信息管理系统');
     expect(await driver.getCurrentUrl())
-        .to.eventually.equal(`${baseURL}/ad_notice.php`)
-        .and.notify(done);
+        .to.equal(`${baseURL}/ad_notice.php`);
 
     await driver.findElement(By.css('#content >' +
       ' div.container-fluid > div >' +
       ' div > a')).click();
 
-    expect(await driver.getTitle()).to.eventually.equal('企业信息管理系统');
+    expect(await driver.getTitle()).to.equal('企业信息管理系统');
     expect(await driver.getCurrentUrl())
-        .to.eventually.equal(`${baseURL}/ad_notice_insert.php`)
-        .and.notify(done);
+        .to.equal(`${baseURL}/ad_notice_insert.php`);
 
     await driver.findElement(By.css('input[name="notify_title"]')).sendKeys('TEST_TITLE');
     await driver.findElement(By.css('textarea[name="notify_content"]')).sendKeys('TEST_CONTENT');
@@ -45,18 +42,16 @@ describe('Add - Inform', () => {
       ' div > div.widget-content.nopadding >' +
       ' form > div.form-actions > button.btn.btn-warning')).click();
 
-    expect(await driver.getTitle()).to.eventually.equal('企业信息管理系统');
+    expect(await driver.getTitle()).to.equal('企业信息管理系统');
     expect(await driver.getCurrentUrl())
-        .to.eventually.equal(`${baseURL}/ad_notice.php`)
-        .and.notify(done);
+        .to.equal(`${baseURL}/ad_notice.php`);
 
     await driver.findElement(By.css('#content >' +
       ' div.container-fluid > div > div > a')).click();
 
-    expect(await driver.getTitle()).to.eventually.equal('企业信息管理系统');
+    expect(await driver.getTitle()).to.equal('企业信息管理系统');
     expect(await driver.getCurrentUrl())
-        .to.eventually.equal(`${baseURL}/ad_notice_insert.php`)
-        .and.notify(done);
+        .to.equal(`${baseURL}/ad_notice_insert.php`);
 
     await driver.findElement(By.css('input[name="notify_title"]')).sendKeys('TEST_TITLE');
     await driver.findElement(By.css('textarea[name="notify_content"]')).sendKeys('TEST_CONTENT');
@@ -70,10 +65,9 @@ describe('Add - Inform', () => {
     await driver.switchTo().alert().accept();
     await driver.switchTo().defaultContent();
 
-    expect(await driver.getTitle()).to.eventually.equal('企业信息管理系统');
+    expect(await driver.getTitle()).to.equal('企业信息管理系统');
     expect(await driver.getCurrentUrl())
-        .to.eventually.equal(`${baseURL}/ad_notice.php`)
-        .and.notify(done);
+        .to.equal(`${baseURL}/ad_notice.php`);
 
     await driver.findElement(By.css('#user-nav>' +
       'ul>li:nth-child(2) a[href="/anli/' +
@@ -86,7 +80,6 @@ describe('Add - Inform', () => {
     await driver.switchTo().defaultContent();
 
     expect(await driver.getCurrentUrl())
-        .to.eventually.equal(`${baseURL}/login_page.php`)
-        .and.notify(done);
+        .to.equal(`${baseURL}/login_page.php`);
   });
 });
--- test/spec/addMonth.js
+++ test/spec/addMonth.js
@@ -2,28 +2,24 @@ import { it, describe } from 'selenium-webdriver/testing';
 import { expect } from 'chai';
 import { By, until } from 'selenium-webdriver';
 
-chai.use(chaiAsPromised);
-
 export default (driver, baseURL) =>
 describe('View - Inform', () => {
   it('View - Inform', async () => {
     await driver.get(`${baseURL}/`);
-    expect(await driver.getTitle()).to.eventually.equal('人力资源管理系统');
+    expect(await driver.getTitle()).to.equal('人力资源管理系统');
     await driver.findElement(By.css('input[name="name"]')).sendKeys('admin');
     await driver.findElement(By.css('input[name="password"]')).sendKeys('admin');
     await driver.findElement(By.css('input[type="submit"]')).click();
-    expect(await driver.getTitle()).to.eventually.equal('企业信息管理系统');
+    expect(await driver.getTitle()).to.equal('企业信息管理系统');
     expect(await driver.getCurrentUrl())
-        .to.eventually.equal(`${baseURL}/ad_home.php`)
-        .and.notify(done);
+        .to.equal(`${baseURL}/ad_home.php`);
 
     await driver.findElement(By.css('#content >' +
       ' div.container-fluid > div > ul >' +
       ' li:nth-child(7) > a')).click();
-    expect(await driver.getTitle()).to.eventually.equal('企业信息管理系统');
+    expect(await driver.getTitle()).to.equal('企业信息管理系统');
     expect(await driver.getCurrentUrl())
-        .to.eventually.equal(`${baseURL}/ad_season.php`)
-        .and.notify(done);
+        .to.equal(`${baseURL}/ad_season.php`);
     await driver.findElement(By.css('#content >' +
       ' div.container-fluid > div >' +
       ' div:nth-child(2) > div >' +
@@ -32,10 +28,9 @@ describe('View - Inform', () => {
     await driver.wait(until.alertIsPresent());
     await driver.switchTo().alert().accept();
     await driver.switchTo().defaultContent();
-    expect(await driver.getTitle()).to.eventually.equal('企业信息管理系统');
+    expect(await driver.getTitle()).to.equal('企业信息管理系统');
     expect(await driver.getCurrentUrl())
-        .to.eventually.equal(`${baseURL}/ad_season.php`)
-        .and.notify(done);
+        .to.equal(`${baseURL}/ad_season.php`);
 
     await driver.findElement(By.css('#user-nav>' +
       'ul>li:nth-child(2) a[href="/anli/logout.php"]')).click();
@@ -43,7 +38,6 @@ describe('View - Inform', () => {
     await driver.switchTo().alert().accept();
     await driver.switchTo().defaultContent();
     expect(await driver.getCurrentUrl())
-        .to.eventually.equal(`${baseURL}/login_page.php`)
-        .and.notify(done);
+        .to.equal(`${baseURL}/login_page.php`);
   });
 });
