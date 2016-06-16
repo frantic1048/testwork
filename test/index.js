71eac657a1d65550a479964f2e88f6c1f1630a1a
nmchgx <nmchgx@163.com>
2016-06-16 19:41:44 +0800

    fix bugs

--- test/index.js
+++ test/index.js
@@ -29,7 +29,6 @@ const targets = {
 * Testing suites
 */
 import simpleGet from './spec/simpleGet';
-<<<<<<< 04442c2d2c6f2e40eb8906ec57d1094d07c2f21b
 import loginLogout from './spec/loginLogout';
 import reportForm from './spec/reportForm';
 
@@ -42,35 +41,19 @@ import editUser from './spec/editUser';// by wjh
 import deleteUser from './spec/deleteUser';// by wjh
 import viewMonth from './spec/viewMonth';// by wjh
 import addMonth from './spec/addMonth';// by wjh
-import homeList from './spec/homeList';
-=======
-// import loginLogout from './spec/loginLogout';
-// import homeListAdmin from './spec/homeListAdmin';
-<<<<<<< b3fddd76459c88c13d857d5aa5436ab9bfd871dd
-<<<<<<< b6c600b0aabdbe7afe0cde918d406fbf9ab7b520
-// import homeListDepartment from './spec/homeListDepartment';
->>>>>>> 完成2.3.1
-=======
+
+import homeListAdmin from './spec/homeListAdmin';
 import homeListDepartment from './spec/homeListDepartment';
->>>>>>> fix bugs
-=======
-// import homeListDepartment from './spec/homeListDepartment';
-<<<<<<< 7ebdd8b644a763be94b907d2407f1da6d6b04a86
 import checkDepartmentRecord from './spec/checkDepartmentRecord';
->>>>>>> 完成2.3.2/2.4.1/2.4.2
-=======
-// import checkDepartmentRecord from './spec/checkDepartmentRecord';
-// import managePerson from './spec/managePerson';
 import applyActivity from './spec/applyActivity';
->>>>>>> 完成2.15和2.16
+import managePerson from './spec/managePerson';
+
 
 /**
  * what test runs
  * @type {Array}
  */
 const suites = [
-<<<<<<< 04442c2d2c6f2e40eb8906ec57d1094d07c2f21b
-<<<<<<< c9660e628be45b701c0d6984bc15ee314ed3da00
   simpleGet,
   loginLogout,
   reportForm,
@@ -83,32 +66,11 @@ const suites = [
   deleteUser, // by wjh
   viewMonth, // by wjh
   addMonth, // by wjh
-=======
-  // simpleGet,
-  // loginLogout,
-  homeList,
->>>>>>> test homeList
-=======
-  simpleGet,
-  // loginLogout,
-  // homeListAdmin,
-<<<<<<< b3fddd76459c88c13d857d5aa5436ab9bfd871dd
-<<<<<<< b6c600b0aabdbe7afe0cde918d406fbf9ab7b520
-  // homeListDepartment,
->>>>>>> 完成2.3.1
-=======
+  homeListAdmin,
   homeListDepartment,
->>>>>>> fix bugs
-=======
-  // homeListDepartment,
-<<<<<<< 7ebdd8b644a763be94b907d2407f1da6d6b04a86
   checkDepartmentRecord,
->>>>>>> 完成2.3.2/2.4.1/2.4.2
-=======
-  // checkDepartmentRecord,
-  // managePerson,
+  managePerson,
   applyActivity,
->>>>>>> 完成2.15和2.16
 ];
 
 /**
